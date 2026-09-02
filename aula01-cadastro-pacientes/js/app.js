0ab40351cdbb50a4a9d690d21031640ca8d35016
aluno <aluno@fateclins.local>
2026-09-01 22:25:37 -0300

    telefone adicionado

--- aula01-cadastro-pacientes/js/app.js
+++ aula01-cadastro-pacientes/js/app.js
@@ -1,13 +1,15 @@
 // Array que guarda os pacientes cadastrados (em memória, só nesta sessão)
 const pacientes = [];
+const hoje = new Date();
+
 
 // Referências aos elementos do DOM que vamos usar várias vezes
 const formulario = document.getElementById('form-paciente');
 const tabela = document.getElementById('tabela-pacientes');
 
 // Função responsável por adicionar um paciente ao array
-function adicionarPaciente(nome, email, nascimento) {
-	const novoPaciente = { nome, email, nascimento };
+function adicionarPaciente(nome, email, nascimento, telefone) {
+	const novoPaciente = { nome, email, nascimento, telefone };
 	pacientes.push(novoPaciente);
 }
 
@@ -22,6 +24,7 @@ function renderizarTabela() {
       <td>${paciente.nome}</td>
       <td>${paciente.email}</td>
       <td>${formatarData(paciente.nascimento)}</td>
+      <td>${paciente.telefone}</td>
     `;
 
 		tabela.appendChild(linha);
@@ -41,8 +44,9 @@ formulario.addEventListener('submit', (event) => {
 	const nome = document.getElementById('nome').value;
 	const email = document.getElementById('email').value;
 	const nascimento = document.getElementById('nascimento').value;
+	const telefone = document.getElementById('telefone').value;
 
-	adicionarPaciente(nome, email, nascimento);
+	adicionarPaciente(nome, email, nascimento,telefone);
 	renderizarTabela();
 
 	formulario.reset(); // limpa os campos do formulário
